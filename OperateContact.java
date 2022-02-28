13907eece21c3e46eafd252ced9db0a4ff41745f
athul_tharol <athultharol1994@gmail.com>
2022-02-28 14:28:49 +0530

    address book program added

---
+++ OperateContact.java
@@ -0,0 +1,80 @@
+package com.bridgelabz.addressbook;
+
+import java.util.Scanner;
+
+import java.util.HashMap;
+
+public class OperateContact{
+
+    HashMap<String, AdressBook> contactMap = new HashMap<>();
+
+    public void editContact(){
+
+        String address;
+        Scanner sc= new Scanner(System.in);
+        System.out.print("\nEnter the First name for update Address : ");
+        String editName = sc.nextLine();
+        int flag = 1;
+        for (String firstName : contactMap.keySet())
+        {
+            AdressBook contact = contactMap.get(firstName);
+
+            if(editName.equals(contact.getFirstName())){
+                System.out.print("\nEnter your new Address : ");
+                address = sc.nextLine();
+                contact.setAddress(address);
+                flag = 0;
+            }
+        }
+        if(flag == 1){
+            System.out.print("\ncouldn't find the contact..");
+        }
+        else{
+            System.out.print("\nYour contact updated.");
+        }
+    }
+    public void deleteContact(){
+
+        Scanner sc= new Scanner(System.in);
+        System.out.print("\nEnter the First name for delete : ");
+        String deleteName = sc.nextLine();
+        int flag = 1;
+        for (String firstName : contactMap.keySet())
+        {
+            AdressBook contact = contactMap.get(firstName);
+            if(deleteName.equals(contact.getFirstName())){
+                contactMap.remove(firstName);
+                flag = 0;
+            }
+        }
+        if(flag == 1){
+            System.out.print("\ncouldn't find the contact..");
+        } 
+        else{
+            System.out.print("\nYour contact deleted..");
+        }
+        sc.close();   
+    }
+    public void showContact(){
+
+        for (String firstName : contactMap.keySet())
+        {
+            AdressBook contact = contactMap.get(firstName);
+
+            System.out.print("\n-----------------");
+            System.out.print("\nFirst Name  : " +  contact.getFirstName());
+            System.out.print("\nLast Name   : " +  contact.getLastName());
+            System.out.print("\nAddress     : " +  contact.getAddress());
+            System.out.print("\nCity        : " +  contact.getCity());
+            System.out.print("\nState       : " +  contact.getState());
+            System.out.print("\nPone Number : " +  contact.getPhoneNo());
+            System.out.print("\nE-mail      : " +  contact.getEmail());
+            System.out.print("\nZip         : " +  contact.getZip());
+        }
+    }
+    public void storeContact(String firstName, AdressBook contact){
+
+        this.contactMap.put(firstName, contact);
+    }
+
+}
